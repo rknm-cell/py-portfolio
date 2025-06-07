37489667cbba20ca3e4c2969a5d32745a5230386
rknm-cell <racknahm@gmail.com>
2025-06-07 12:12:22 -0400

    added artist statement

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,12 +11,12 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
+    default: 'Peter Yelton Portfolio',
     template: '%s | Next.js Portfolio Starter',
   },
   description: 'This is my portfolio.',
   openGraph: {
-    title: 'My Portfolio',
+    title: 'Peter Yelton Portfolio',
     description: 'This is my portfolio.',
     url: baseUrl,
     siteName: 'My Portfolio',
--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,10 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Peter Yelton's Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm Peter Yelton, this is my portfolio website showcasing my work and my writings as an artist in the tech world.`}
       </p>
       <div className="my-8">
         <BlogPosts />
